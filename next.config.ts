52ef3c8b608d8162583fe9633ce71b7e6ae9ce41
naveenalla3000 <naveenalla3000@gmail.com>
2025-06-12 17:06:59 +0530

    sam

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,10 @@ const nextConfig: NextConfig = {
     // Enable type checking during build
     ignoreBuildErrors: true,
   },
+  eslint: {
+    // Allow builds even if there are ESLint errors
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
